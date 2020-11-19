8e8d4d349420cd5d31c7c900961f0b48ee1ad37a
Waldri <waldri.so@gmail.com>
2020-11-19 16:32:16 +0000

    first commit openCVcpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: waldri
+ *
+ * Created on 16 de novembro de 2020, 16:20
+ */
+#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+
+using namespace cv;
+using namespace std;
+
+ 
+int main( )
+{
+ 
+       Mat image;
+ 
+       // LOAD image
+       image = imread("/home/waldri/catkin_ws/src/ros_essentials_cpp/src/topic03_perception/images/chess.jpg", cv::IMREAD_COLOR);   // Read the file "image.jpg".
+              //This file "image.jpg" should be in the project folder.
+              //Else provide full address : "D:/images/image.jpg"
+ 
+       if(! image.data )  // Check for invalid input
+       {
+              cout <<  "Could not open or find the image" << std::endl ;
+              return -1;
+       }
+ 
+       //DISPLAY image
+       namedWindow( "window", WINDOW_AUTOSIZE ); // Create a window for display.
+       imshow( "window", image ); // Show our image inside it.
+ 
+       //SAVE image
+       imwrite("/home/waldri/catkin_ws/src/ros_essentials_cpp/src/topic03_perception/images/copy/chess.jpg",image);// it will store the image in name "result.jpg"
+ 
+       waitKey(0);                       // Wait for a keystroke in the window
+       return 0;
+}
