5f9457a83217461a9f468111e26812879b78c96d
Waldri <waldri.so@gmail.com>
2020-11-24 12:08:30 +0000

    24/11 manhã

--- assignment.cpp
+++ assignment.cpp
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * 24/11/2020
+ * 24/11/2020 manhã
  */ 
 
 #include "opencv2/opencv.hpp"
